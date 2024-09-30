021753a9f180135d83295ddeabc6d293f951ef95
iceman23e <57103374+iceman23e@users.noreply.github.com>
2024-09-29 22:20:49 -0400

    Update install.sh
    updated for prompts

--- install.sh
+++ install.sh
@@ -7,12 +7,13 @@ prompt() {
     local prompt_message="$1"
     local default_value="$2"
     local var_name="$3"
-
-    read -e -p "$prompt_message [$default_value]: " input
+    
+    echo "$prompt_message [$default_value]: "
+    read input
     if [[ -z "$input" ]]; then
         input="$default_value"
     fi
-    declare "$var_name=$input"
+    eval "$var_name=\"$input\""
 }
 
 # Prompt for variables
